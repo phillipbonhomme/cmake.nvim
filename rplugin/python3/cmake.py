257ecde2a587900896b698e1c5b65c6c34cfe3ae
Phillip Bonhomme <phillip.bonhomme@gmail.com>
2017-08-10 18:33:25 -0700

    Cleanup and add unit test.

--- rplugin/python3/cmake.py
+++ rplugin/python3/cmake.py
@@ -1,81 +1,86 @@
 import neovim
 from pathlib import Path
 import subprocess
-#import os
+
+# 1. Determine if Clean or Dirty
+#    a. Clean - Goto Step #2
+#    b. Dirty - Delete all Old Build Files
+# 2. Run CMake Project Configuration
+#    a. Passes - Goto Step #3
+#    b. Fails - Delete all Old Build Files
+# 3. Setup RTags Daemon
+#    a. Passes - Goto Step #4
+#    b. Fails - Kill Daemon & Repeat Step #3
+# 4. Setup RTags Client
+#    a. Passes - Done!
+#    b. Fails - Kill Daemon & Exit
+cmake_build_info = {
+    "old_cmake_files": [Path("CMakeCache.txt"), Path("cmake_install.cmake")],
+    "old_cmake_dir": Path("CMakeFiles"),
+    "cmake_proj": Path("CMakeLists.txt"),
+    "makefile": Path("Makefile"),
+    "build_dir": Path("build"),
+    "comp_data_cmake": Path("build/compile_commands.json"),
+    "comp_data_bear": Path("compile_commands.json")}
+
+cmake_cmd_info = {
+    "cmake_cmd": ["cmake", "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON", ".."],
+    "rdm_cmd": ["rdm", "--silent", "--daemon"],
+    "rc_cmd": ["rc", "-J", "build"]}
+
 
 @neovim.plugin
-class Main(object):
+class CMake(object):
     def __init__(self, vim):
         self.vim = vim
-        self.old_cmake_files = [ Path("CMakeCache.txt"), Path("cmake_install.cmake") ]
-        self.old_cmake_dir = Path("CMakeFiles")
-        self.cmake_proj = Path("CMakeLists.txt")
-        self.makefile = Path("Makefile")
-        self.build_dir = Path("build")
-        self.comp_data_cmake = Path("build/compile_commands.json")
-        self.comp_data_bear = Path("compile_commands.json")
 
     def run_cmake(self):
         try:
-            subprocess.call( ["mkdir", "build"] )
+            subprocess.call(["mkdir", "build"])
         except OSError:
             self.vim.command('echo "Can\'t setup CMake build directory."')
             raise
         try:
-            self.vim.command('echo "Running CMake..."')
-            subprocess.call( ["cmake", "-DCMAKE_EXPORT_COMPILE_COMMANDS=1", ".."], cwd="build" )
+            subprocess.call([self.cmake_cmd], cwd="build")
         except OSError:
             self.vim.command('echo "CMake Failed."')
             raise
-        if self.comp_data_cmake.is_file():
+        if cmake_build_info["comp_data_cmake"].is_file():
             try:
-                subprocess.call( ["rdm", "--silent", "--daemon"], cwd=".." )
+                subprocess.call([self.rdm_cmd], cwd="..")
             except OSError:
                 self.vim.command('echo "Couldn\'t start the RTags daemon."')
                 raise
-            subprocess.call( ["rc", "-J", "build"] )
+            subprocess.call([self.rc_cmd])
         else:
-            self.vim.command('echo "Error Generating Compilation Database With CMake"')
+            self.vim.command(
+                'echo "Error Generating Compilation Database With CMake"')
             raise
 
-#    def run_bear(self):
-#        try:
-#            subprocess.call(["bear", "make"])
-#        except OSError as e:
-#            if e.errno == os.errno.ENOENT:
-#                self.vim.command('echo "No Makefile for Bear to Use"')
-#            else:
-#                self.vim.command('echo "Bear Error"')
-#            raise
-#        if self.comp_data_bear.is_file():
-#            subprocess.call(["rdm", "--log-file=$HOME/dev/logs/rdm.log", "--silent", "--daemon"])
-#            subprocess.call(["rc", "-J", "."])
-#        else:
-#            self.vim.command('echo "Error Generating Compilation Database With Bear"')
-#
     @neovim.function('CMakeCompDB')
     def cMakeCompDB(self, args):
         self.vim.command('echo "Starting CMake Project"')
 
-        if self.old_cmake_dir.is_dir():
-            subprocess.call( ["rm", "-rf", str( old_cmake_dir ) ] )
+        if cmake_build_info["old_cmake_dir"].is_dir():
+            subprocess.call(
+                ["rm", "-rf", str(cmake_build_info["old_cmake_dir"])])
 
-        if self.comp_data_bear.is_file():
-            subprocess.call( ["rm", str( comp_data_bear ) ] )
+        if cmake_build_info["comp_data_bear"].is_file():
+            subprocess.call(["rm", str(cmake_build_info["comp_data_bear"])])
 
-        for path in self.old_cmake_files:
+        for path in cmake_build_info["old_cmake_files"]:
             if path.is_file():
-                subprocess.call(["rm", str( path )])
+                subprocess.call(["rm", str(path)])
 
-        if self.cmake_proj.is_file():
-            if self.build_dir.is_dir():
+        if cmake_build_info["cmake_proj"].is_file():
+            if cmake_build_info["build_dir"].is_dir():
                 self.vim.command('echo "Cleaning up Build Directory"')
                 subprocess.call(["rm", "-rf", "build"])
             self.vim.command('echo "Running CMake"')
             self.run_cmake()
         else:
             self.vim.command('echo "Not a CMake Project"')
-            if self.makefile.is_file():
+            if cmake_build_info["makefile"].is_file():
                 self.run_bear()
             else:
                 self.vim.command('echo "Not Setup for Autotools Either"')
