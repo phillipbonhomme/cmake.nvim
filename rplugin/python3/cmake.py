d163c691f160835c0c11ae0ed660b644f146caf6
Phillip Bonhomme <phillip.bonhomme@gmail.com>
2017-08-12 11:19:38 -0700

    Pull methods out of neovim plugin object.

--- rplugin/python3/cmake.py
+++ rplugin/python3/cmake.py
@@ -18,80 +18,80 @@ cmake_cmd_info = {
     "rdm_cmd": ["rdm", "--silent", "--daemon"],
     "rc_cmd": ["rc", "-J", str(cmake_build_info["build_dir"])]}
 
+def removeDirtyDir(args):
+    if cmake_build_info["build_dir"].is_dir():
+        print('echo "Cleaning up Build Directory"')
+        subprocess.call(
+            ["rm", "-rf", str(cmake_build_info["build_dir"])])
+
+def removeOldCMakeFiles(args):
+    if cmake_build_info["old_cmake_dir"].is_dir():
+        print('echo "Cleaning up Old CMake Directory"')
+        subprocess.call(
+            ["rm", "-rf", str(cmake_build_info["old_cmake_dir"])])
+    for path in cmake_build_info["old_cmake_files"]:
+        if path.is_file():
+            print('echo "Cleaning up Old CMake Files"')
+            subprocess.call(["rm", str(path)])
+
+def run_cmake(args):
+    print('echo "Running CMake"')
+    try:
+        subprocess.check_call(["mkdir", "build"])
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+        print('echo "Can\'t setup CMake build directory."')
+        raise
+
+    try:
+        subprocess.check_call([cmake_cmd_info["cmake_cmd"]], cwd="build")
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+        print('echo "CMake Failed."')
+        raise
+    else:
+        print(
+            'echo "Error Generating Compilation Database With CMake"')
+        raise
+
+def setup_rtags_daemon(args):
+    print('echo "Initializing RTags Daemon"')
+    try:
+        subprocess.check_call([cmake_cmd_info["rdm_cmd"]], cwd="..")
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+        print('echo "Couldn\'t start the RTags daemon."')
+        raise
+
+def connect_rtags_client(args):
+    print('echo "Connecting RTags Client"')
+    if cmake_build_info["comp_data_cmake"].is_file():
+        try:
+            subprocess.check_call([cmake_cmd_info["rc_cmd"]])
+        except subprocess.CalledProcessError as e:
+            print(e.output)
+            print('echo "Couldn\'t connect the RTags client."')
+            raise
+    else:
+        print(
+            'echo "Error Generating Compilation Database With CMake"')
+
 
 @neovim.plugin
 class Main(object):
     def __init__(self, vim):
         self.vim = vim
 
-    def removeDirtyDir(self):
-        if cmake_build_info["build_dir"].is_dir():
-            self.vim.command('echo "Cleaning up Build Directory"')
-            subprocess.call(
-                ["rm", "-rf", str(cmake_build_info["build_dir"])])
-
-    def removeOldCMakeFiles(self):
-        if cmake_build_info["old_cmake_dir"].is_dir():
-            self.vim.command('echo "Cleaning up Old CMake Directory"')
-            subprocess.call(
-                ["rm", "-rf", str(cmake_build_info["old_cmake_dir"])])
-        for path in cmake_build_info["old_cmake_files"]:
-            if path.is_file():
-                self.vim.command('echo "Cleaning up Old CMake Files"')
-                subprocess.call(["rm", str(path)])
-
-    def run_cmake(self):
-        self.vim.command('echo "Running CMake"')
-        try:
-            subprocess.check_call(["mkdir", "build"])
-        except subprocess.CalledProcessError as e:
-            print(e.output)
-            self.vim.command('echo "Can\'t setup CMake build directory."')
-            raise
-
-        try:
-            subprocess.check_call([cmake_cmd_info["cmake_cmd"]], cwd="build")
-        except subprocess.CalledProcessError as e:
-            print(e.output)
-            self.vim.command('echo "CMake Failed."')
-            raise
-        else:
-            self.vim.command(
-                'echo "Error Generating Compilation Database With CMake"')
-            raise
-
-    def setup_rtags_daemon(self):
-        self.vim.command('echo "Initializing RTags Daemon"')
-        try:
-            subprocess.check_call([cmake_cmd_info["rdm_cmd"]], cwd="..")
-        except subprocess.CalledProcessError as e:
-            print(e.output)
-            self.vim.command('echo "Couldn\'t start the RTags daemon."')
-            raise
-
-    def connect_rtags_client(self):
-        self.vim.command('echo "Connecting RTags Client"')
-        if cmake_build_info["comp_data_cmake"].is_file():
-            try:
-                subprocess.check_call([cmake_cmd_info["rc_cmd"]])
-            except subprocess.CalledProcessError as e:
-                print(e.output)
-                self.vim.command('echo "Couldn\'t connect the RTags client."')
-                raise
-        else:
-            self.vim.command(
-                'echo "Error Generating Compilation Database With CMake"')
-
     @neovim.function('CMakeCompDB', sync=True)
     def CMakeCompDB(self):
-        self.removeOldCMakeFiles()
+        removeOldCMakeFiles([])
         if cmake_build_info["build_dir"].is_dir():
-            self.removeDirtyDir()
+            removeDirtyDir([])
 
         if cmake_build_info["cmake_proj"].is_file():
-            self.vim.command('echo "Starting CMake Project"')
-            self.run_cmake()
-            self.setup_rtags_daemon()
-            self.connect_rtags_client()
+            vim.command('echo "Starting CMake Project"')
+            run_cmake([])
+            setup_rtags_daemon([])
+            connect_rtags_client([])
         else:
             self.vim.command('echo "Not a CMake Project"')
--- tests/rplugin/python3/test_cmake.py
+++ tests/rplugin/python3/test_cmake.py
@@ -38,21 +38,26 @@ class TestCMake(utTestCase):
         #    print("Can't Launch Neovim in test fixture setup.")
         #    raise
         print("Setup for CMake unit test")
-        if nvim_remote_socket.is_file():
-            subprocess.call(["rm", str(nvim_remote_socket)])
-        subprocess.call(["touch", str(nvim_remote_socket)])
-        os.environ["NVIM_LISTEN_ADDRESS"] = str(nvim_remote_socket)
-        self.nvimproc = subprocess.Popen(
-            ["nvim", "--headless", "-c", "\"terminal python3\""])
-        nvim = neovim.attach('socket', path=NVIM_LISTEN_ADDRESS)
-        self.cmake_plugin = cmake.Main(nvim)
+        #if nvim_remote_socket.is_file():
+        #    subprocess.call(["rm", str(nvim_remote_socket)])
+        #subprocess.call(["touch", str(nvim_remote_socket)])
+        #os.environ["NVIM_LISTEN_ADDRESS"] = str(nvim_remote_socket)
+        #self.nvimproc = subprocess.Popen(
+        #    ["nvim", "--headless", "-c", "\"terminal python3\""])
+        #nvim = neovim.attach('socket', path=NVIM_LISTEN_ADDRESS)
+        #self.cmake_plugin = cmake.Main(nvim)
 
     def tearDown(self):
         print("Teardown for CMake unit test")
-        self.nvimproc.terminate()
-        del os.environ["NVIM_LISTEN_ADDRESS"]
-        if nvim_remote_socket.is_file():
-            subprocess.call(["rm", str(nvim_remote_socket)])
+        try:
+            os.chdir("../../../..")
+        except OSError:
+            print("Test Error: Couldn't cd out of test directory")
+            raise
+        #self.nvimproc.terminate()
+        #del os.environ["NVIM_LISTEN_ADDRESS"]
+        #if nvim_remote_socket.is_file():
+        #    subprocess.call(["rm", str(nvim_remote_socket)])
 
     def test_InitClean(self):
         #build_area = utMock()
@@ -63,12 +68,24 @@ class TestCMake(utTestCase):
             raise
         for path in cmake.cmake_build_info["old_cmake_files"]:
             self.assertFalse(path.is_file())
-        self.assertFalse(cmake.cmake_build_info["old_cmake_dir"].is_file())
+        self.assertFalse(cmake.cmake_build_info["old_cmake_dir"].is_dir())
+
+    def test_initDirty(self):
+        subprocess.call(["ls"])
         try:
-            os.chdir("tests/rplugin/python3")
+            os.chdir("tests/rplugin/python3/dirty")
         except OSError:
-            print("Test Error: Couldn't cd out of 'clean'")
+            print("Test Error: Couldn't cd into 'dirty'")
             raise
+        try:
+            os.chdir(str(cmake.cmake_build_info["build_dir"]))
+        except OSError:
+            print("Test Error: Couldn't cd into build directory")
+            raise
+        for path in cmake.cmake_build_info["old_cmake_files"]:
+            if path != Path("compile_commands.json"):
+                self.assertTrue(path.is_file())
+        self.assertTrue(cmake.cmake_build_info["old_cmake_dir"].is_dir())
 
 
 if __name__ == '__main__':
