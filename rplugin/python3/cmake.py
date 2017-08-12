b0ce1580f9936ef98345bca59a9c08b5ff78b9cc
Phillip Bonhomme <phillip.bonhomme@gmail.com>
2017-08-12 13:31:19 -0700

    Don't try and catch exceptions generated during CMake.

--- rplugin/python3/cmake.py
+++ rplugin/python3/cmake.py
@@ -29,61 +29,64 @@ cmake_cmd_info = {
 
 def removeDirtyDir():
     if cmake_build_info["build_dir"].is_dir():
-        print('echo "Cleaning up Build Directory"')
+        print("Cleaning up Build Directory")
         subprocess.call(["rm", "-rf", str(cmake_build_info["build_dir"])])
 
 
 def removeOldCMakeFiles():
     if cmake_build_info["old_cmake_dir"].is_dir():
-        print('echo "Cleaning up Old CMake Directory"')
+        print("Cleaning up Old CMake Directory")
         subprocess.call(["rm", "-rf", str(cmake_build_info["old_cmake_dir"])])
     for path in cmake_build_info["old_cmake_files"]:
         if path.is_file():
-            print('echo "Cleaning up Old CMake Files"')
+            print("Cleaning up Old CMake Files")
             subprocess.call(["rm", str(path)])
 
 
 def run_cmake():
-    print('echo "Running CMake"')
+    print("Running CMake")
     try:
         subprocess.check_call(["mkdir", "build"])
     except subprocess.CalledProcessError as e:
         print(e.output)
-        print('echo "Can\'t setup CMake build directory."')
+        print("Can\'t setup CMake build directory.")
         raise
 
-    try:
-        subprocess.check_call(cmake_cmd_info["cmake_cmd"], cwd="build")
-    except subprocess.CalledProcessError as e:
-        print(e.output)
-        print('echo "CMake Failed."')
-        raise
-    else:
-        print('echo "Error Generating Compilation Database With CMake"')
-        raise
+    subprocess.check_call(cmake_cmd_info["cmake_cmd"], cwd="build")
+    if not cmake_build_info["comp_data_cmake"].is_file():
+        print("Couldn't setup CMake Project")
+    #try:
+    #    subprocess.check_call(cmake_cmd_info["cmake_cmd"], cwd="build")
+    #except subprocess.CalledProcessError as e:
+    #    print(e.output)
+    #    print("CMake Failed.")
+    #    raise
+    #else:
+    #    print("Error Generating Compilation Database With CMake")
+    #    raise
 
 
 def setup_rtags_daemon():
-    print('echo "Initializing RTags Daemon"')
+    print("Initializing RTags Daemon")
     try:
         subprocess.check_call(cmake_cmd_info["rdm_cmd"], cwd="..")
     except subprocess.CalledProcessError as e:
         print(e.output)
-        print('echo "Couldn\'t start the RTags daemon."')
+        print("Couldn\'t start the RTags daemon.")
         raise
 
 
 def connect_rtags_client():
-    print('echo "Connecting RTags Client"')
+    print("Connecting RTags Client")
     if cmake_build_info["comp_data_cmake"].is_file():
         try:
             subprocess.check_call(cmake_cmd_info["rc_cmd"])
         except subprocess.CalledProcessError as e:
             print(e.output)
-            print('echo "Couldn\'t connect the RTags client."')
+            print("Couldn\'t connect the RTags client.")
             raise
     else:
-        print('echo "Error Generating Compilation Database With CMake"')
+        print("Error Generating Compilation Database With CMake")
 
 
 @neovim.plugin
